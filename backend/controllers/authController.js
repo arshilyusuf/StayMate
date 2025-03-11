1f39f8fe1d66d6e9047c57711723925ed54866d8
arshilyusuf <arshilyusuf@gmail.com>
2025-03-11 11:10:32 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -60,7 +60,7 @@ exports.login = catchAsync(async (req, res, next) => {
     .cookie("jwt", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV==='production',
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
